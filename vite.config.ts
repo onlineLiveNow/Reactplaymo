146857605ae16df3a9618acfaa87386164c7afbb
onlineLiveNow <playammmo@gmail.com>
2025-12-25 20:29:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/', // Default to root for most platforms
+  base: '/', // Make sure this is just '/' for Netlify
 })
